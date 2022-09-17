142e8301912887c803824c1cf3751edb0292bd15
hienpham <peterparker.pvh@gmail.com>
2022-09-17 19:03:11 +0700

    send mail

--- js/21880038.js
+++ js/21880038.js
@@ -1,4 +1,5 @@
 const API = 'https://web1-api.herokuapp.com/api';
+const AUTHENTICATE_API = 'https://web1-api.herokuapp.com/users';
 async function loadData(request,templateId,viewId){
     const response = await fetch(`${API}/${request}`);
     const data = await response.json();
@@ -8,4 +9,20 @@ async function loadData(request,templateId,viewId){
     var view = document.getElementById(viewId);
     view.innerHTML = template(context);  
     
+}
+
+async function getAuthenticateToken(username,password){
+    let response = await fetch(`${AUTHENTICATE_API}/authenticate`,{
+        method :'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        },
+        body : JSON.stringify({username,password})
+    });
+    let result = await response.json();
+    if(response.status == 200){
+        return result.token;
+    }
+    throw new Error(result.message);
 }
